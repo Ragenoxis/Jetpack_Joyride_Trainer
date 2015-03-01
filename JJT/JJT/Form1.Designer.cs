3b2a90de114eaab3bc34a9b7413a91066928ba56
Cem Bilen <Ragenoxis@gmail.com>
2015-03-01 23:43:08 +0200

    Jetpack Joyride Trainer
    An open source "Jetpack Joyride" C# Trainer

---
+++ JJT/JJT/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace JJT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 80);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(272, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(269, 17);
+            this.toolStripStatusLabel1.Text = "JJT(Jetpack Joyride Trainer) - Copyright Ragenoxis";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(184, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Get Golds";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(77, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Amount of gold :\r\nMAX : 9999";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Amount of token :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(102, 44);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(77, 26);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(185, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 28);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Get Tokens";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(272, 102);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JJT V1.2";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+
+    }
+}
+
---
+++ JJT/JJT/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using ProcessMemoryReaderLib;
+using System.Runtime.InteropServices;
+
+namespace JJT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Gold()
+        {
+            try
+            {
+                ProcessMemoryReaderLib.ProcessMemoryReader preader = new ProcessMemoryReaderLib.ProcessMemoryReader();
+                ProcessMemoryReader Reader = new ProcessMemoryReader();
+                Process[] MyProcess = Process.GetProcessesByName("JetpackJoyride_Win8");
+                preader.ReadProcess = MyProcess[0];
+                preader.OpenProcess();
+
+                int byteswritten;
+                long value;
+                byte[] memory;
+                string wadress = "010D73C8";
+                IntPtr writeadress = new IntPtr(Convert.ToInt32(wadress, 16));
+
+                value = Convert.ToInt64(textBox1.Text);
+                memory = BitConverter.GetBytes(value);
+                preader.WriteProcessMemory((IntPtr)writeadress, memory, out byteswritten);
+                MessageBox.Show("Value has been successfully injected to Jetpack Joyride !");
+            }
+            catch
+            {
+                MessageBox.Show("Something goes wrong !");
+            }
+        }
+
+        public void Token()
+        {
+            try
+            {
+                ProcessMemoryReaderLib.ProcessMemoryReader preader = new ProcessMemoryReaderLib.ProcessMemoryReader();
+                ProcessMemoryReader Reader = new ProcessMemoryReader();
+                Process[] MyProcess = Process.GetProcessesByName("JetpackJoyride_Win8");
+                preader.ReadProcess = MyProcess[0];
+                preader.OpenProcess();
+
+                int byteswritten;
+                long value;
+                byte[] memory;
+                string wadress = "010D73D0";
+                IntPtr writeadress = new IntPtr(Convert.ToInt32(wadress, 16));
+
+                value = Convert.ToInt64(textBox2.Text);
+                memory = BitConverter.GetBytes(value);
+                preader.WriteProcessMemory((IntPtr)writeadress, memory, out byteswritten);
+                MessageBox.Show("Value has been successfully injected to Jetpack Joyride !");
+            }
+            catch
+            {
+                MessageBox.Show("Something goes wrong !");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Gold();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Token();
+        }
+    }
+}
